0482adf750fc2f6ced9706f05a20778f8ace6624
EC2 Default User <ec2-user@ip-172-31-23-35.ap-northeast-1.compute.internal>
2022-03-28 17:24:10 +0000

    first commit

---
+++ db/migrate/20220328165321_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+
+      t.string :name
+      t.string :body
+      t.string :title
+
+      t.timestamps
+    end
+  end
+end
